141c5277093be425d4253bf213e333a34d6a2bf6
pgj472 <pgj472@alumni.ku.dk>
2024-05-04 12:54:49 +0200

    Latest update

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -7,4 +7,33 @@ import matplotlib.pyplot as plt
 # We start by creating a class called cournot which will be used to create our functions
 class cournot:
     # We define the cost function:
-    
+    def cost_function(self, qi, MC):
+        return MC * qi
+
+    # We define the profit function for firm 1:
+    def profit_function1(self, q1, q2, a, b, MC):
+        Q = q1 + q2
+        p = a - b * Q
+        return p * q1 - self.cost_function(q1, MC)
+
+    # We define the profit function for firm 2:
+    def profit_function2(self, q1, q2, a, b, MC):
+        Q = q1 + q2
+        p = a - b * Q
+        return p * q2 - self.cost_function(q2, MC)
+
+    # We define the first order condition for firm 1:
+    def foc1(self, q1, q2, a, b, MC):
+        return a - 2 * b * q1 - b * q2 - MC
+
+    # We define the first order condition for firm 2:
+    def foc2(self, q1, q2, a, b, MC):
+        return a - b * q1 - 2 * b * q2 - MC
+
+    # We define the best response function for firm 1:
+    def br1(self, q2, a, b, MC):
+        return (a - MC) / (2 * b) - q2 / 2
+
+    # We define the best response function for firm 2:
+    def br2(self, q1, a, b, MC):
+        return (a - MC) / (2 * b) - q1 / 2
