1f8d5836bd13e5c2588752937817fe29b64e9161
pgj472 <pgj472@alumni.ku.dk>
2024-06-22 09:27:18 +0200

    Latest

--- examproject/examproject.py
+++ examproject/examproject.py
@@ -1,4 +1,32 @@
+from types import SimpleNamespace
+import matplotlib.pyplot as plt
+from scipy.optimize import root
+from scipy import optimize
+from scipy.optimize import minimize
+import pandas as pd
+import numpy as np
 
+
+class productioneconomy:
+    #Defining parameters:
+    def __init__(self):
+
+        par = self.par = SimpleNamespace()
+
+        # a. preferences
+        par.alpha = 1/3
+        par.beta = 2/3
+
+        # b. endowments
+        par.w1A = 0.8
+        par.w2A = 0.3
+        par.w1B = 0.2
+        par.w2B = 0.7
+
+    #Defining utility functions:
+    def utility_A(self,x1A,x2A):
+        return(x1A**self.par.alpha)*x2A**(1-self.par.alpha)
+   
 
 
 
